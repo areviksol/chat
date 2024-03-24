abb75ad73ec1aba501d1a86b5275f3ed066c80cd
Arevik Mkrtchyan <arevik.mkrtchian1997@gmail.com>
2024-03-24 19:15:52 +0400

    fix bugs

--- client.py
+++ client.py
@@ -2,18 +2,40 @@ import socket
 import sys
 from threading import Thread
 
+server_disconnected = False
+
 def receive_messages(client_socket):
+    global server_disconnected
     while True:
-        data = client_socket.recv(1024).decode()
-        if not data:
-            print("Server disconnected")
+        try:
+            data = client_socket.recv(1024).decode()
+            if not data:
+                print("Server disconnected")
+                server_disconnected = True
+                break
+            print(data)
+        except Exception as e:
+            print("Error occurred:", e)
             break
-        print("Received message:", data)
 
 def send_messages(client_socket):
     while True:
-        message = input("Enter your message: ")
-        client_socket.send(message.encode())
+        try:
+            if server_disconnected:
+                break
+            if not client_socket.fileno() == -1:
+                message = input("")
+                client_socket.send(message.encode())
+            else:
+                break
+        except OSError as e:
+            print("Error occurred:", e)
+            break
+
+def thread_cleanup(client_socket):
+    client_socket.close()
+    sys.exit()
+
 
 def main():
     if len(sys.argv) != 3:
@@ -22,25 +44,39 @@ def main():
 
     server_ip = sys.argv[1]
     port = int(sys.argv[2])
-
+    
+    
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
+        name = input("Enter your name: ") 
         client_socket.connect((server_ip, port))
         print("Connected to server at {}:{}".format(server_ip, port))
+        client_socket.send(name.encode())
 
         receive_thread = Thread(target=receive_messages, args=(client_socket,))
-        receive_thread.start()
-
+        try:
+            receive_thread.daemon =True
+            receive_thread.start()
+        except:
+            print("Error occurred during thread start:", e)
+            thread_cleanup(client_socket)
+            raise e
         send_thread = Thread(target=send_messages, args=(client_socket,))
-        send_thread.start()
-
+        try:
+            send_thread.daemon =True
+            send_thread.start()
+        except:
+            print("Error occurred during thread start:", e)
+            thread_cleanup(client_socket)
+            raise e
         receive_thread.join()
         send_thread.join()
-        
-    except Exception as e:
-        print("Error:", e)
-    finally:
-        client_socket.close()
+    except ConnectionRefusedError:
+        print("Server is not running.") 
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt detected. Exiting client.")
+    except SystemExit:
+        print("SystemExit detected. Exiting client.")
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import socket
 import sys
+import threading
 from threading import Thread
+import signal
+import select
+
+clients = []
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def send_all(clients, sender, message):
     for client in clients:
@@ -8,54 +14,68 @@ def send_all(clients, sender, message):
             client.send(message.encode())
 
 class ClientHandler(Thread):
-    def __init__(self, client_socket, addr, clients):
+    def __init__(self, client_socket, addr, clients, name):
         Thread.__init__(self)
         self.client_socket = client_socket
         self.addr = addr
         self.clients = clients
+        self.client_name = name
 
     def run(self):
         while True:
-            data = self.client_socket.recv(1024).decode()
-            if not data:
-                print("Client {} disconnected".format(self.addr))
+            try:
+                data = self.client_socket.recv(1024).decode()
+                if not data:
+                    print("Client {} disconnected".format(self.addr))
+                    self.client_socket.close()
+                    self.clients.remove(self.client_socket)
+                    break
+
+                print("Received message from {}: {}".format(self.addr, data))
+
+                message = "{}: {}".format(self.client_name, data)
+                send_all(self.clients, self.client_socket, message)
+            except Exception as e:
+                print("Error occurred:", e)
                 self.client_socket.close()
-                self.clients.remove(self.client_socket)
+                if self.client_socket in self.clients:
+                    self.clients.remove(self.client_socket)
                 break
 
-            print("Received message from {}: {}".format(self.addr, data))
-
-            message = "client {}: {}\n".format(self.clients.index(self.client_socket), data)
-            send_all(self.clients, self.client_socket, message)
-
 def main():
-
+    global s
     if len(sys.argv) != 2:
         print("Usage: python server.py <port>")
         return
 
-    host = '0.0.0.0'
+    host = "0.0.0.0"
     port = int(sys.argv[1])
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     s.bind((host, port))
     s.listen(5)
 
     print("Server listening on {}:{}".format(host, port))
-
-    clients = []
-
     while True:
-        client_socket, addr = s.accept()
-        print("Got connection from", addr)
+        try:
+            client_socket, addr = s.accept()
+            print("Got connection from", addr)
+            name = client_socket.recv(1024).decode()
+            print("Client name:", name)
 
-        clients.append(client_socket)
+            clients.append(client_socket)
 
-        message = "server: client {} just arrived\n".format(len(clients))
-        send_all(clients, client_socket, message)
+            message = "server: client {} just arrived\n".format(name)
+            send_all(clients, client_socket, message)
 
-        handler = ClientHandler(client_socket, addr, clients)
-        handler.start()
+            handler = ClientHandler(client_socket, addr, clients, name)
+            handler.daemon = True
+            handler.start()
+        except KeyboardInterrupt:
+            print("\nKeyboardInterrupt detected. Exiting loop.")
+            s.close()
+            break
+    print("After finally....")
 
 if __name__ == "__main__":
     main()
